96c4cd758c41af119b86d2fb64b6840836571310
komalarao <manoharvakamullu1999@gmail.com>
2023-01-06 17:38:18 -0800

    added: welcome message for logical java programs

---
+++ src/com/bridgelabz/logical_programs/WelcomeMessage.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.logical_programs;
+
+public class WelcomeMessage {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to java logical programs");
+
+	}
+
+}
